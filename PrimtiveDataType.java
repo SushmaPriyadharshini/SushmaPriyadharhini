138f69e43364bba26bc66425dc6d4d971b3cddbf
SushmaPriyadharshini <sushma.nks@gmail.com>
2024-05-23 07:22:44 +0530

    Add files via upload
    week1.day2

---
+++ PrimtiveDataType.java
@@ -0,0 +1,18 @@
+package Week1.Day2;
+
+public class PrimtiveDataType {
+	
+	public static void main(String[] args) {
+		float browseVersion = 100.2f;
+		String browserName = "Firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char broswerLogo='f';
+		System.out.println(browseVersion);
+		System.out.println(browserName);
+		System.out.println(isVisible);
+		System.out.println(releaseYear);
+		System.out.println(broswerLogo);
+	}
+
+}
